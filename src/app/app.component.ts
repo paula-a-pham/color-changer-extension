111fcc3bbaa31f8078d646b448fdcd18d2c01bd6
Paula Ashraf <paula.a.pham@gmail.com>
2024-11-06 11:15:03 +0200

    update app component TypeScript file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+/// <reference types="chrome" />
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'color-changer';
