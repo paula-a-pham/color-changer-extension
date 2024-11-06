818409a603f2dbfcc2eb646c59970eb11f82795a
Paula Ashraf <paula.a.pham@gmail.com>
2024-11-06 12:03:42 +0200

    messaging between background and content

--- src/extension/background.ts
+++ src/extension/background.ts
@@ -6,6 +6,29 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'COLOR_CHANGE_REQUEST') {
+    try {
+      chrome.tabs.sendMessage(
+        request.tabId,
+        { action: 'CHANGE_PAGE_COLOR' },
+        (response) => {
+          if (chrome.runtime.lastError) {
+            console.error(
+              'Error sending message to content : ',
+              chrome.runtime.lastError
+            );
+            alert('Error sending message to content please try again');
+            return;
+          }
+        }
+      );
+    } catch (error) {
+      console.error('Unexpected error : ', error);
+
+      sendResponse({
+        status: false,
+        errorMessage: 'Unexpected error occurred.',
+      });
+    }
   }
   return true;
 });
